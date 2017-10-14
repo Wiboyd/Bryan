4308dbe614c844b2b5ce152858723ed92bb811a7
Arnaud Miguet <arnaud.miguet74@orange.fr>
2017-10-14 11:35:39 +0200

    Initial Commit

---
+++ Bryan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bryan
+//
+//  Created by Arnaud Miguet on 14/10/2017.
+//  Copyright © 2017 Arnaud Miguet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
