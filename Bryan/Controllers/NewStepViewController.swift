f5c806446696b33e2948f5471fdc6584e83b7d6e
Arnaud Miguet <arnaud.miguet74@orange.fr>
2017-10-14 19:08:15 +0200

    Add Behavior for step text field (remove all markup)

---
+++ Bryan/Controllers/NewStepViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewStepViewController.swift
+//  Bryan
+//
+//  Created by Arnaud Miguet on 14/10/2017.
+//  Copyright © 2017 Arnaud Miguet. All rights reserved.
+//
+
+import UIKit
+
+class NewStepViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet var stepTextView: UITextView!
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "") {
+            if ((self.stepTextView.text as NSString).substring(with: range) == "]") {
+                
+                var count = 1
+                for i in (0..<(range.location - 1)).reversed() {
+                    count += 1;
+                    if(self.stepTextView.text[self.stepTextView.text.index(self.stepTextView.text.startIndex, offsetBy: i)] == "[") {
+                        break;
+                    }
+                }
+                
+                let newRange = NSRange(location: 0, length: range.location - count)
+                self.stepTextView.text = (self.stepTextView.text as NSString).substring(with: newRange)
+                
+                return false;
+            }
+        }
+        return true
+    }
+    
+
+}
